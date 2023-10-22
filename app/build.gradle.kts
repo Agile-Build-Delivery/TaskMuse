9b02a043b240466abf1fa3da0710310c8788c8bd
psavarmattas <puranjaysavarmattas@gmail.com>
2023-10-22 22:40:48 +0100

    TAS-14 Firebase Setup and New Workflow
    - Theme name error fixed in app/src/main/res/values-night/themes.xml
    - Firestore Database iis added.
    - Firebase Authentication is added.
    - Firebase TestLab is added in the .github/workflows/gradle_build.yaml to upload and test the app on Pixel 8 Pro with 34 API level.
    - Firebase Analytics is added.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services") // Google services Plugin
 }
 
 android {
@@ -39,4 +40,12 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // FIREBASE PACKAGES
+    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
+    implementation("com.google.firebase:firebase-auth:22.2.0")
+    implementation("com.google.firebase:firebase-firestore")
+    implementation ("com.google.firebase:firebase-database")
 }
