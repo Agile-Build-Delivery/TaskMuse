526dee5289768ed9e63fc8e3325c0a5cb4b92093
Kajal Singh <singh.kajal940@gmail.com>
2023-10-30 17:55:31 +0000

    Reorganize project directories and move files for a cleaner structure 🧹🧹

--- app/src/main/java/com/taskmuse/app/ui/activity/MainActivity/MainActivity.java
+++ app/src/main/java/com/taskmuse/app/ui/activity/MainActivity/MainActivity.java
@@ -1,27 +1,13 @@
-package com.example.taskmuse;
+package com.taskmuse.app.ui.activity.MainActivity;
 
-import static android.content.ContentValues.TAG;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.analytics.FirebaseAnalytics;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.taskmuse.app.R;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
