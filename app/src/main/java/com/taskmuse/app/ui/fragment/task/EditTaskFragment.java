4a135c44cf9a0438acd0cfe4b4f2d319f14ab1c4
Kajal Singh <singh.kajal940@gmail.com>
2023-11-19 16:25:51 +0000

    XML layout of Edit Task Page🎨 ✨
    This commit introduces the XML layout for the Edit Task page. The layout includes fields for Project Name, Task Name, Priority, Description, and Task Assignee. Each input field is accompanied by a label for better accessibility, and appropriate styling, such as dropdown borders, has been applied. The priority and project name inputs are set as dropdowns, and a multiline text input is provided for the task description. This layout is designed to enhance the user experience when editing tasks.

---
+++ app/src/main/java/com/taskmuse/app/ui/fragment/task/EditTaskFragment.java
@@ -0,0 +1,78 @@
+package com.taskmuse.app.ui.fragment.task;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.fragment.app.Fragment;
+
+import com.taskmuse.app.R;
+
+public class EditTaskFragment extends Fragment {
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+    public EditTaskFragment() {
+        // Required empty public constructor
+    }
+
+    public static EditTaskFragment newInstance(String param1, String param2) {
+        EditTaskFragment fragment = new EditTaskFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_edit_task, container, false);
+
+        // Assuming you have an array of project names
+        String[] projectNames = {"Project A", "Project B", "Project C"};
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_spinner_item, projectNames);
+
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Get the Spinner reference and set the adapter
+        Spinner projectNameSpinner = rootView.findViewById(R.id.projectNameSpinner);
+        projectNameSpinner.setAdapter(adapter);
+
+
+        // Assuming you have an array of priority levels
+        String[] priorities = {"Higher", "High", "Medium", "Low", "Lowest"};
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<String> priorityAdapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_spinner_item, priorities);
+
+        // Specify the layout to use when the list of choices appears
+        priorityAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Get the Spinner reference and set the adapter
+        Spinner prioritySpinner = rootView.findViewById(R.id.prioritySpinner);
+        prioritySpinner.setAdapter(priorityAdapter);
+
+
+        return rootView;
+    }
+}
