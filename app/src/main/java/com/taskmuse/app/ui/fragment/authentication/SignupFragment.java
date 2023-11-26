4d0867a8b083cb91788394bdb40930bfc51706f4
Agata Duda <A00260823@student.ait.ie>
2023-11-26 19:57:17 +0000

    Fix: remove unused imports

--- app/src/main/java/com/taskmuse/app/ui/fragment/authentication/SignupFragment.java
+++ app/src/main/java/com/taskmuse/app/ui/fragment/authentication/SignupFragment.java
@@ -2,10 +2,7 @@ package com.taskmuse.app.ui.fragment.authentication;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,7 +13,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -28,7 +24,6 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.taskmuse.app.R;
 import com.taskmuse.app.ui.activity.MainActivity.MainActivity;
-import com.taskmuse.app.ui.fragment.get_started.GetStartedFragment;
 import com.taskmuse.app.utils.ValidationUtils;
 
 public class SignupFragment extends Fragment {
